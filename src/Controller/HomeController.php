2b67648080899288d92a29fa51ac726d9e54e5fb
Karine <k.challier@yahoo.fr>
2022-02-02 22:12:44 +0100

    create contact form and render

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormTypeInterface;
 
 class HomeController extends AbstractController
 {
@@ -18,6 +22,5 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
-
 
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'Email',
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('min' => 3)))
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Message',
+                'constraints' => array(
+                    new NotBlank(),
+                    new Length(array('min' => 3)),
+                ),
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
